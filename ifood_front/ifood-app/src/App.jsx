3cbf0c2129bcca022cfabb870afd3b244b3923cf
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2023-12-29 23:11:51 -0300

    feat(init): create React project with Vite and tailwind

---
+++ ifood_front/ifood-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import RoutesApp from "./routes"
+
+
+
+function App() {
+
+    return (
+      <div className='border-cian-300 border-2 h-screen max-h-screen'>
+        <RoutesApp />
+      </div>
+  )
+}
+
+export default App
