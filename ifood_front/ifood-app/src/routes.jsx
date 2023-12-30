d0706fdb3d5afdc05293cec9fad718c982ab4b99
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2023-12-30 00:39:29 -0300

    feat(routes): implementar estrutura inicial de rotas

---
+++ ifood_front/ifood-app/src/routes.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Home";
+import AlbumList from "./pages/Album";
+import Sobre from "./pages/Sobre";
+import NotFound from "./pages/NotFound";
+
+export default function RoutesApp() {
+    return (
+        <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Main />}/>
+            <Route path="/albuns" element={<AlbumList />} />
+            <Route path="/sobre" element={<Sobre />} />
+
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        </BrowserRouter>
+    )
+}
