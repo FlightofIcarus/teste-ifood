08ee0896c63fb25207299503f56ddebf55a8423f
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-01-05 20:50:39 -0300

    feat(componentes de suporte): Adicionar componentes para visualização de rotas.

---
+++ ifood_front/ifood-app/src/components/Emphasis.jsx
@@ -0,0 +1,10 @@
+export default function empashis ({list}){
+    
+    let listajson = list;
+    // console.log(listajson);
+    return (
+        <section>
+            
+            <img src="https://via.placeholder.com/150/92c952" alt="imagem" />
+        </section>);
+}
