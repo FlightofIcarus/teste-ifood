f513860a02ada07d1ed91611be153fad1d0f0a22
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-01-05 20:13:30 -0300

    chore: Migrar de Gradle para Maven, alterando projeto backend

--- ifood_back/ifood-app/src/test/kotlin/icarosantos/ifood/TesteApplicationTests.kt
+++ ifood_back/ifood-app/src/test/kotlin/icarosantos/ifood/TesteApplicationTests.kt
@@ -1,10 +1,10 @@
-package com.example.demo
+package icarosantos.ifood
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class IfoodrestfullApplicationTests {
+class TesteApplicationTests {
 
 	@Test
 	fun contextLoads() {
